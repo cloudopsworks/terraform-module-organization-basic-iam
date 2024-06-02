5fe3b81c5cfa47350ba1701d33b4b3754943c49f
Cristian Beraha <cristian@cloudops.works>
2024-06-01 21:17:18 -0300

    fix: missing variable + validation

--- modules/terraform-role/variables.tf
+++ modules/terraform-role/variables.tf
@@ -15,3 +15,11 @@ variable "is_org" {
   type    = bool
   default = false
 }
+
+variable "organization_id" {
+  type = string
+  default = ""
+  validation {
+    condition = var.is_org
+    error_message = "Organization ID is required when is_org is set to true"}
+}
