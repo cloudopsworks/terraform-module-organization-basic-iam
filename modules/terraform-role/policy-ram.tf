d6ec892e3599c1bf03c30d9175868281e0bd9c40
Cristian Beraha <cristian@cloudops.works>
2024-06-06 10:55:06 -0300

    chore: RAM admin permission

---
+++ modules/terraform-role/policy-ram.tf
@@ -0,0 +1,19 @@
+# RAM policy access
+data "aws_iam_policy_document" "tf_ram_policy_admin" {
+  version = "2012-10-17"
+
+  statement {
+    sid = "RAMAdminRole"
+    effect = "Allow"
+    actions = [
+      "ram:*"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "terraform_access_ram_admin" {
+  name   = "RAMAdmin"
+  role   = aws_iam_role.terraform_access.name
+  policy = data.aws_iam_policy_document.tf_ram_policy_admin[0].json
+}
