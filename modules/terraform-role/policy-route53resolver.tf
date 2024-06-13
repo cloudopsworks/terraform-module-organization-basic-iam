6454bc2b715c59e9ea86bd93162213ad645d5913
Cristian Beraha <cristian@cloudops.works>
2024-06-13 00:30:58 -0300

    feat: terraform additional rule

---
+++ modules/terraform-role/policy-route53resolver.tf
@@ -0,0 +1,17 @@
+data "aws_iam_policy_document" "tf_route53_admin" {
+  version = "2012-10-17"
+
+  statement {
+    sid    = "Route53ResolverAll"
+    effect = "Allow"
+    actions = [
+      "route53resolver:*"
+    ]
+    resources = ["*"]
+  }
+}
+resource "aws_iam_role_policy" "terraform_access_route53resolver_admin" {
+  name   = "Route53ResolverAdminAccess"
+  role   = aws_iam_role.terraform_access.name
+  policy = data.aws_iam_policy_document.tf_route53_resolver_admin.json
+}
