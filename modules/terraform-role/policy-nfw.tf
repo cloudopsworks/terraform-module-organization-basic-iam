56cf0088f2917f633046a82d5dfdfb8fa7c67889
Cristian Beraha <cristian@cloudops.works>
2024-06-27 23:23:33 -0300

    chore: addition of NetworkFirewall IAM

---
+++ modules/terraform-role/policy-nfw.tf
@@ -0,0 +1,37 @@
+# Network Admin Policy
+data "aws_iam_policy_document" "tf_nfw_admin" {
+  count   = var.is_org ? 1 : 0
+  version = "2012-10-17"
+
+  statement {
+    sid     = "AllowGlobal"
+    effect  = "Allow"
+    actions = [
+      "network-firewall:ListRuleGroups"
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "AllowAllObjects"
+    effect = "Allow"
+    actions = [
+      "network-firewall:*"
+    ]
+    resources = [
+      "arn:aws:network-firewall:*:${var.account_id}:firewall-policy/*",
+      "arn:aws:network-firewall:*:${var.account_id}:stateless-rulegroup/*",
+      "arn:aws:network-firewall:*:${var.account_id}:stateful-rulegroup/*",
+      "arn:aws:network-firewall:*:${var.account_id}:tls-configuration/*",
+      "arn:aws:network-firewall:*:${var.account_id}:firewall/*"
+    ]
+
+  }
+}
+
+resource "aws_iam_role_policy" "terraform_access_nfw_admin" {
+  count  = var.is_org ? 1 : 0
+  name   = "NetworkFirewallAdmin"
+  role   = aws_iam_role.terraform_access.name
+  policy = data.aws_iam_policy_document.tf_nfw_admin[0].json
+}
