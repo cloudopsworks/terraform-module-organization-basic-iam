c0a400500392e15755f1bccf6341506a569679b4
Cristian Beraha <cristian@cloudops.works>
2024-08-20 19:02:10 -0300

    chore: ecr policy admin

---
+++ modules/terraform-role/policy-ecr.tf
@@ -0,0 +1,44 @@
+# EKS Admin Policy
+data "aws_iam_policy_document" "tf_ecr_admin" {
+  version = "2012-10-17"
+  statement {
+    sid    = "ECRAccess"
+    effect = "Allow"
+    actions = [
+      "ecr:GetRegistryPolicy",
+      "ecr:DescribeRegistry",
+      "ecr:DescribePullThroughCacheRules",
+      "ecr:DescribeRepositoryCreationTemplates",
+      "ecr:GetAuthorizationToken",
+      "ecr:UpdateRepositoryCreationTemplate",
+      "ecr:PutRegistryScanningConfiguration",
+      "ecr:CreatePullThroughCacheRule",
+      "ecr:DeletePullThroughCacheRule",
+      "ecr:PutRegistryPolicy",
+      "ecr:GetRegistryScanningConfiguration",
+      "ecr:ValidatePullThroughCacheRule",
+      "ecr:CreateRepositoryCreationTemplate",
+      "ecr:BatchImportUpstreamImage",
+      "ecr:DeleteRepositoryCreationTemplate",
+      "ecr:DeleteRegistryPolicy",
+      "ecr:UpdatePullThroughCacheRule",
+      "ecr:PutReplicationConfiguration",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    sid     = "EKSComponents"
+    effect  = "Allow"
+    actions = ["eks:*"]
+    resources = [
+      "arn:aws:ecr:*:${var.account_id}:repository/*",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "terraform_access_ecr_admin" {
+  name   = "ECRAdmin"
+  role   = aws_iam_role.terraform_access.name
+  policy = data.aws_iam_policy_document.tf_ecr_admin.json
+}
